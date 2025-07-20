5aa823e812442aaef602cba41857ae68e2adc0bc
MuhammadMurodov2006 <153983963+MuhammadMurodov2006@users.noreply.github.com>
2025-07-20 11:27:35 +0500

    Updated app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import streamlit as st
 from fastai.vision.all import *
-
-
-# import pathlib
-# temp = pathlib.PosixPath
-# pathlib.PosixPath = pathlib.WindowsPath
-
+import platform
+import pathlib
+plt = platform.system()
+if plt == 'Linux':
+    pathlib.PosixPath = pathlib.WindowsPath
 #title
 st.title('Image Classification with FastAI')
 
